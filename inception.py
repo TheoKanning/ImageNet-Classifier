c06210b6cb05f6b3619bd0a34b3d184d22ef1be2
Theo Kanning <theokanning@gmail.com>
2016-04-15 19:54:49 -0500

    Added 2 more layers, greatly improved speed, still not converging

--- inception.py
+++ inception.py
@@ -3,7 +3,7 @@ from input_data import create_datasets
 import numpy as np
 import tensorflow as tf
 
-NUM_IMAGES = 10
+NUM_IMAGES = 100
 
 classes = np.array([["dog", "n02084071"],
                     ["cat", "n02121808"],
@@ -26,6 +26,7 @@ classes = np.array([["dog", "n02084071"],
                     ["door", "n03222176"]
                     ])
 
+classes = classes[3:5]
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -37,8 +38,8 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(x, W, strides):
+    return tf.nn.conv2d(x, W, strides=strides, padding='SAME')
 
 
 def max_pool_2x2(x):
@@ -55,59 +56,73 @@ y_ = tf.placeholder(tf.float32, shape=[None, num_classes])
 
 x_reshaped = tf.reshape(x, [-1, input_data.IMAGE_WIDTH, input_data.IMAGE_HEIGHT, 3])
 
-# First convolutional layer
-W_conv1 = weight_variable([5, 5, 3, 32])
-b_conv1 = bias_variable([32])
+# First convolutional layer, (224, 224, 3) to (56, 56, 16)
+W_conv1 = weight_variable([11, 11, 3, 16])
+b_conv1 = bias_variable([16])
 
-h_conv1 = tf.nn.relu(conv2d(x_reshaped, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+h_conv1 = tf.nn.relu(conv2d(x_reshaped, W_conv1, [1, 4, 4, 1]) + b_conv1)
 
-# Second convolutional layer
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+# Second convolutional layer, (56, 56, 16) to (14, 14, 32)
+W_conv2 = weight_variable([5, 5, 16, 32])
+b_conv2 = bias_variable([32])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, [1, 2, 2, 1]) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-# First fully-connected layer
-W_fc1 = weight_variable([56 * 56 * 64, 1024])
-b_fc1 = bias_variable([1024])
+# Third convolutional layer, (14, 14, 32) to (7, 7, 64)
+W_conv3 = weight_variable([3, 3, 32, 64])
+b_conv3 = bias_variable([64])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 56 * 56 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3, [1, 1, 1, 1]) + b_conv3)
+h_pool3 = max_pool_2x2(h_conv3)
+
+# First fully-connected layer
+W_fc1 = weight_variable([7 * 7 * 64, 512])
+b_fc1 = bias_variable([512])
+
+h_pool3_flat = tf.reshape(h_pool3, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # Second fully-connected layer
-W_fc2 = weight_variable([1024, num_classes])
-b_fc2 = bias_variable([num_classes])
+W_fc2 = weight_variable([512, 128])
+b_fc2 = bias_variable([128])
 
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
+h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
+
+# Third fully-connected layer
+W_fc3 = weight_variable([128, num_classes])
+b_fc3 = bias_variable([num_classes])
+
+y_conv = tf.nn.softmax(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
 
 # Training
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess = tf.Session()
 sess.run(tf.initialize_all_variables())
+
 for i in range(20000):
     image_batch, label_batch = train_dataset.next_batch(50)
+    sess.run(train_step, feed_dict={x: image_batch, y_: label_batch, keep_prob: 0.5})
     if i % 5 == 0:
         train_accuracy = sess.run(accuracy, feed_dict={
             x: image_batch, y_: label_batch, keep_prob: 1.0})
         print("step %d, training accuracy %g" % (i, train_accuracy))
     else:
         print("step %d" % i)
-    sess.run(train_step, feed_dict={x: image_batch, y_: label_batch, keep_prob: 0.5})
 
 print("test accuracy %g" % sess.run(accuracy, feed_dict={
     x: test_dataset.images, y_: test_dataset.labels, keep_prob: 1.0}))
 
 
 def main():
-    ids = classes[:, 1]
+    ids = classes[:2, 1]
     input_data.download_dataset(ids, NUM_IMAGES)
 
 
